c43bc0f6bb34e158cbe56400088122d3bf0b44ac
msakenis <mindaugas@pitchground.com>
2021-07-04 14:37:22 +0300

    setup and test

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
+import React, {useContext} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import * as S from './Home.module.scss';
+import {ArticlesListContext} from '../../contexts/ArticlesList.context';
+import {SearchInput, Notification, ArticlesList, Loading, Pagination} from '../../components';
 
 const Home = () => {
+  const {isLoading, articlesList, setUrl, page, totalArticles, query} = useContext(ArticlesListContext);
+
   return (
-    <div>
-      hello
-      <Button color={'primary'}>dfsdf</Button>
-    </div>
+    <Container>
+      <Notification />
+      <SearchInput
+        onRequestSearch={(value) => setUrl({query: value, page: 1})}
+        onCancelSearch={() => setUrl({page: 1})}
+      />
+      <Typography variant={'h4'} component={'h1'} color={'textSecondary'} className={S.title}>
+        Articles
+      </Typography>
+      {isLoading || !articlesList ? (
+        <div className={S.center}>
+          <Loading />
+        </div>
+      ) : (
+        <ArticlesList />
+      )}
+      <Pagination
+        currentPage={page}
+        isLastPage={Math.ceil(totalArticles / 9) === page}
+        isLoading={isLoading}
+        onChange={(newPageValue) => setUrl({query, page: newPageValue})}
+      />
+    </Container>
   );
 };
-
-Home.propTypes = {};
 
 export default Home;
