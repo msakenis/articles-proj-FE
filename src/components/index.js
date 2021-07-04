57adc05d8a0eac79e4308350662edf2062798e12
msakenis <mindaugas@pitchground.com>
2021-07-04 14:37:53 +0300

    export components from one file

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 export {default as Loading} from './Loading/Loading';
+export {default as SearchInput} from './SearchInput/SearchInput';
+export {default as Notification} from './Notification/Notification';
+export {default as ArticlesList} from './ArticlesList/ArticlesList';
+export {default as SingleArticleCard} from './SingleArticleCard/SingleArticleCard';
+export {default as SourceBox} from './SourceBox/SourceBox';
+export {default as Pagination} from './Pagination/Pagination';
