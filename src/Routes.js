d0d97b9cf97a3122b50be7ed081cfd0c16fdb32a
msakenis <mindaugas@pitchground.com>
2021-07-04 14:33:20 +0300

    global state + fetch data

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,20 @@
 import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Loading} from './components';
+import ArticlesListProvider from './contexts/ArticlesList.context';
 
 const Homelazy = lazy(() => import('./pages/Home/Home'));
 
 function Routes() {
   return (
     <Router>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          <Route exact path='/' component={Homelazy} />
-        </Switch>
-      </Suspense>
+      <ArticlesListProvider>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path='/' component={Homelazy} />
+          </Switch>
+        </Suspense>
+      </ArticlesListProvider>
     </Router>
   );
 }
